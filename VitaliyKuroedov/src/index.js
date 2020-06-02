12eb4af2eecd399019c89bd94ed93ef14d7cff74
Rastrapon <58354560+Rastraponovich@users.noreply.github.com>
2020-06-02 23:54:35 +0300

    lesson 1_add_button

--- VitaliyKuroedov/src/index.js
+++ VitaliyKuroedov/src/index.js
@@ -8,15 +8,39 @@ const messages = [
     {name: 'Petr' , content: 'Hi'},
     {name: 'Vitalii' , content: 'Hola'},
     {name: 'Oleg' , content: 'Shalom'},
-
 ]
+
+const App = () => (
+    <MessageList messages={messages}/>
+)
 
 const Message = ({name, content}) => <li><strong>{name}</strong> : {content}</li>
 
+const addMessage = () => {
+    let nameField = document.getElementById('name')
+    let textField = document.getElementById('text')
+    messages.push({name: nameField.value, content: textField.value})
+    ReactDom.render(<App/>, root)
+}
+
 const MessageList = ({messages}) => (
-    <ul>
-        {messages.map((item, index) => <Message {...item} key={index}/> )}
-    </ul>
+    <div>
+        <label>
+            <span>Имя</span>
+            <input id="name" type="text"></input>
+        </label>
+        <label>
+            <span>Текст</span>
+            <input id="text" type="text"></input>
+        </label>
+        <Button name="Добавить" cb={addMessage}/>
+
+        <ul>
+            {messages.map((item, index) => <Message {...item} key={index}/> )}
+        </ul>
+    </div>
 )
 
-ReactDom.render(<MessageList messages={messages}/>, root)
+const Button = ({name, cb}) =>  <button onClick={cb}>{name}</button>
+
+ReactDom.render(<App/>, root)
