4262447ac530d9c7f5b9b535a4dc5642dca89206
Arina S <sladrina@gmail.com>
2020-06-03 17:25:41 +0300

    Add btn

--- ArinaRybak/src/index.js
+++ ArinaRybak/src/index.js
@@ -9,9 +9,28 @@ const messages = [
 ]
 
 const Message = ({ name, content }) => <li><strong>{name}:</strong> {content}</li>
+
+const Button = ()=>{
+    return(
+        <button onClick={BtnClick}>PUSH ME</button>
+    )
+}
+
+function BtnClick(){
+    let addMess = {
+        name: 'Ivan',
+        content: 'Glad to hear u man'
+    }
+    messages.push(addMess)
+    console.log(messages)
+    //В массив сообщение после нажатия добавляется, но так как мы не обновили стейт - не отображается.
+    //Нужно немного переписать код и задать componentDidUpdate, но не в моих правилах бежать впереди паровоза :)
+}
+
 const MessageList = ({ messages }) => (
     <ul>
         {messages.map((item, index) => <Message {...item} key={index} />)}
+        <Button />
     </ul>
 )
 
