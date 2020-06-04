e8f48ec5d33c032aea25fc9ddeb4230634b4435c
Rastrapon <58354560+Rastraponovich@users.noreply.github.com>
2020-06-04 12:26:03 +0300

    chat list added

--- VitaliyKuroedov/src/components/ChatList.jsx
+++ VitaliyKuroedov/src/components/ChatList.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
-
+import Avatar from 'material-ui/Avatar'
+import {List, ListItem} from 'material-ui/List'
+import Subheader from 'material-ui/Subheader'
+import Divider from 'material-ui/Divider'
+import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble'
 
 export default class ChatList extends React.Component {
     render() {
         return(
             <aside className="chat-list">
-                <h2>ChatList</h2>
+                <List>
+                    <Subheader>Последние Чаты</Subheader>
+                    <ListItem
+                        primaryText="Сушист"
+                        leftAvatar={<Avatar src="https://v0.material-ui.com/images/ok-128.jpg"/>}
+                        rightIcon={<CommunicationChatBubble />}/>
+                    <ListItem
+                        primaryText="Визажист"
+                        leftAvatar={<Avatar src="https://v0.material-ui.com/images/kolage-128.jpg"/>}
+                        rightIcon={<CommunicationChatBubble />}/>
+                    <ListItem
+                        primaryText="Бухгалтер"
+                        leftAvatar={<Avatar src="https://v0.material-ui.com/images/raquelromanp-128.jpg"/>}
+                        rightIcon={<CommunicationChatBubble />}/>
+                    <ListItem
+                        primaryText="Качок"
+                        leftAvatar={<Avatar src="https://v0.material-ui.com/images/kerem-128.jpg"/>}
+                        rightIcon={<CommunicationChatBubble />}/>
+               
+                </List>
 
             </aside>
         )
