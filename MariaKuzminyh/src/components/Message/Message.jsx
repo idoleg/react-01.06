07645ae553728c936938ba6533701a6a76650b56
Kuzminyh Maria <kuzminyh.mn@yandex.ru>
2020-06-10 23:31:06 +0500

    MariaKuzminyh. Погружение в React и UI-библиотека

--- MariaKuzminyh/src/components/Message/Message.jsx
+++ MariaKuzminyh/src/components/Message/Message.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames'; 
 
-export function Message(props) {
-  const { name, content } = props;
-  return <li><strong>{ name }:</strong> {content}</li>
+import { ROBOT_NAME } from '../../containers/ChatContainer';
+import './message.css';
+
+export const Message = ({ name, content }) => {
+  const classNameRobot = name === ROBOT_NAME ? 'message_robot' : '';
+  const classes = classNames('message', classNameRobot);
+
+  return (<li className={ classes }><strong>{ name }:</strong> {content}</li>);
+};
+
+Message.propTypes = {
+  name: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired
 }
