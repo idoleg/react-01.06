16bdf65df81c73d5a97b4d5f20f368392d9a42e4
Evgeniya <ved-evgeniya@yandex.ru>
2020-06-18 11:40:32 +0300

    MessageList.jsx

--- EvgeniyaKarpova/src/components/MessagesList/MessagesList.jsx
+++ EvgeniyaKarpova/src/components/MessagesList/MessagesList.jsx
@@ -1,47 +1,16 @@
-import React, {Component} from "react";
-import Message from "../Message/Message"
+import './MessageList.css';
 
-// export const MessagesList = ({messages}) => (
-//     <ul>
-//        {messages.map((item, index) => <Message {...item} key={index}/>)}
-//     </ul>
-// ); // ...item  =  name={item.name} content={item.content}
+import React from "react";
+import {Message} from "../Message/Message";
+import PropTypes from "prop-types";
 
-export default class MessagesList extends Component {
-    state = {
-        //messages: ["Привет!", "Как дела?"]
-        messages: [
-                {name: 'Лена', content: 'Превет!'},
-                {name: 'Оля', content: 'Превет!'},
-                {name: 'Лена', content: 'Как дела?'},
-                {name: 'Оля', content: 'Хорошо'}
-            ]
-    };
- 
-    handleClick = () => {
-        const lena = {name: 'Лена', content: 'Нормально!'};
-        this.setState({ messages: [...this.state.messages, lena] });
-        
-    };
- 
-    render() {
-        const messageElements = this.state.messages.map((text, index) => (
-            <Message key={ index } text={ text } />));
- 
-        return <div>
-            { messageElements }
-            <button onClick={ this.handleClick }>Отправить сообщение</button>
-        </div>
-    };
-    componentDidUpdate() {
-        if (this.state.messages.length % 2 === 1) {  // Остаток от деления на 2
-            const robot = {name: 'Pобот', content: 'Не приставай ко мне!'};
-            setTimeout(() =>
-            this.setState(
-                { messages: [ ...this.state.messages, robot ] }),
-            1000);
-        };
-    }
- 
- }
- 
+export const MessagesList = ({messages}) => (
+    <ul className="message-list">
+        {messages.length == 0 && <strong>Здесь нет сообщений. <br/>Напиши сообщение!</strong>}
+       {messages.map((item, index) => <Message {...item} key={index}/>)}
+    </ul>
+); // ...item  =  name={item.name} content={item.content}
+
+MessagesList.propTypes = {
+    messages: PropTypes.arrayOf(PropTypes.shape(Message.propTypes)).isRequired,
+}
