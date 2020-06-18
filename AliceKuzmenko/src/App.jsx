6bceb5317596384450a34503cdc82d6953560c17
komalapa <komalapa@mail.ru>
2020-06-18 16:04:18 +0400

    Фактически заглушка

--- AliceKuzmenko/src/App.jsx
+++ AliceKuzmenko/src/App.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
-import {ChatContainer} from './containers/ChatContainer/ChatContainer';
-import {ChatList} from './components/ChatList/ChatList'
-import {Profile} from './components/Profile/Profile'
-import {Header} from './components/Header/Header'
+import  ChatContainer from './containers/ChatContainer/ChatContainer';
+import { ChatList } from './components/ChatList/ChatList'
+import { Profile } from './components/Profile/Profile'
+import { Header } from './components/Header/Header'
 
-import {BrowserRouter,Switch, Route} from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
+import { Provider } from 'react-redux'
 
-export const App = () =>{
-   //<ChatContainer/>
-   
-return    <BrowserRouter>
+import { initStore } from './store'
 
-    <Switch>
-        <Route path="/chats">
-            <Header />
-            <ChatList  />
+import {initChats, sendMessage} from './store/chatActions'
+
+const store = initStore();
+
+store.dispatch(initChats());
+store.dispatch(sendMessage(1,'Lorem',"New"))
+store.dispatch(sendMessage(1,'Lorem',"New"))
+// store.dispatch({type:'COUNT',payload:{number:1}})
+// store.dispatch({type:'COUNT',payload:{number:2}})
+// store.dispatch({type:'COUNT',payload:{number:10}})
+// store.dispatch({type:'COUNT',payload:{number:-12}})
+export const App = () => (
+    <Provider store={store}>
+        <BrowserRouter>
+
             <Switch>
-                <Route path="/chats" exact component={ChatContainer} />
-                <Route path="/chats/:id" exact component={ChatContainer} />
+                <Route path="/chats">
+                    <Header />
+                    <ChatList />
+                    <Switch>
+                        <Route path="/chats" exact component={ChatContainer} />
+                        <Route path="/chats/:id" exact component={ChatContainer} />
+                    </Switch>
+                </Route>
+                <Route path="/about">About</Route>
+                <Route path="/contacts">Contact us</Route>
+                <Route path="/profile" exact component={Profile} />
+                <Route path="/">404</Route>
             </Switch>
-        </Route>
-        <Route path="/about">About</Route>
-        <Route path="/contacts">Contact us</Route>
-        <Route path="/profile" exact component={Profile}/>
-        <Route path="/">404</Route>
-    </Switch>
-</BrowserRouter>
-}
+        </BrowserRouter>
+    </Provider>
+)
---
+++ AliceKuzmenko/src/store/chatReducer.js
@@ -0,0 +1,59 @@
+// export default (state ={countedNumber :0},action) => {
+//     if (action.type=='COUNT'){
+//         console.log (action.payload.number, state.countedNumber);
+//         let newNumber=state.countedNumber +action.payload.number;
+//         return {countedNumber:newNumber}      
+//     }
+//     console.log(state)
+//     return state;
+// }
+
+import {handleActions} from "redux-actions"
+import {initChats, sendMessage} from './chatActions'
+
+
+const initialState={};
+export default handleActions({
+    [initChats]:(state,action)=>{
+        //console.log("initChats",state,action)
+        return {
+            1: {
+                name: 'Lorem',
+                messages: [
+                    { name: 'Me', content: 'Hello!', id: 1 },
+                    { name: 'Lorem', content: 'Hi!', id: 2 },
+                    { name: 'Me', content: 'How are you?', id: 3 },
+                    { name: 'Lorem', content: 'Fine', id: 4 },
+                ]
+            },
+            2: {
+                name: 'Ipsum',
+                messages: [
+                    { name: 'Me', content: 'Hello!', id: 1 },
+                    { name: 'Ipsum', content: 'Hi!', id: 2 },
+                ]
+            },
+            3: {
+                name: 'Dolor',
+                messages: []
+            },
+            //4:{name:'Sit'},
+            //5:{name:'Amet'},    
+        };
+    },
+    [sendMessage]:(state,action)=>{
+        //console.log(state,action)
+        const { id, name, content } = action.payload;
+        let message={name:name, content:content, id:(state[id].messages.length+1)}
+        return {
+            ...state,
+            [id]: {
+                ...state[id],
+                messages: [
+                    ...state[id].messages,message
+                ]
+            }
+        };
+    }
+
+},initialState)
