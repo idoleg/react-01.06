1ed956101135c1c8920d888e5139e8e7c9e2364a
Evgeniya <ved-evgeniya@yandex.ru>
2020-06-18 11:39:06 +0300

    Header.jsx

---
+++ EvgeniyaKarpova/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+import React from "react"; 
+import PropTypes from "prop-types";
+export const Header = ({charId}) => {
+    
+return <div className="header">
+   Чат {charId} </div>;
+   
+}
+
+
+ 
