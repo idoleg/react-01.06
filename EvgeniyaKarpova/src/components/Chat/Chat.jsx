7efa883aa2d0b30a206bc26e4896a10d42b3bd2e
Evgeniya <ved-evgeniya@yandex.ru>
2020-06-22 13:36:29 +0300

    chat

---
+++ EvgeniyaKarpova/src/components/Chat/Chat.jsx
@@ -0,0 +1,25 @@
+import './Chat.css'
+
+import React from 'react';
+import {MessagesList} from '../MessagesList/MessagesList';
+import {ChatForm} from '../ChatForm/ChatForm';
+import PropTypes from 'prop-types';
+import {ChatList} from '../ChatList/ChatList';
+import {Header} from '../Header/Header';
+
+export const Chat = ({messages, onSendMessage, charId}) => (
+   
+    <main>
+        <Header charId={charId}/>  
+        <div className="chatBlok">
+            <ChatList />
+            {!messages && <strong className="chatNet">Такого чата нет!</strong>}
+            {messages && <MessagesList messages={messages} />}
+        </div>
+       {messages && <ChatForm onSendMessage={onSendMessage} />}
+    </main> 
+);
+
+Chat.propTypes = {
+    onSendMessage: PropTypes.func.isRequired,
+}
