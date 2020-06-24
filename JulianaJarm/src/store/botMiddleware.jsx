bd6b7ab26a238793fe72d07f1b73f46a36b56e23
julianajarm <julianajarm@gmail.com>
2020-06-24 17:33:31 +0300

    bot message in every new chat added

--- JulianaJarm/src/store/botMiddleware.jsx
+++ JulianaJarm/src/store/botMiddleware.jsx
@@ -1,4 +1,4 @@
-import { sendMessage } from "./chatActions";
+import { sendMessage, createChat } from "./chatActions";
 
 // export default function (store) {
 //     return function (next) {
@@ -15,18 +15,29 @@ const timeoutIds = {};
 export default store => next => action => {
     next(action);
 
-    if(action.type === sendMessage.toString()) {
+    if (action.type === sendMessage.toString()) {
         const {name, id} = action.payload;
 
         if (name !== BOT_NAME) {
             clearTimeout(timeoutIds[id]);
             timeoutIds[id] = setTimeout(generateBotAnswer, 3000, store, id, name);
         }
+    } else if (action.type === createChat.toString()) {
+        const {id} = action.payload;
+        generateBotMessageInNewChat(store, id);
+        console.log(id)
     }
 }
+
 
 function generateBotAnswer (store, id, name) {
     const chatName = store.getState().chats[id].name;
     const content = `Hey, ${name}, I'm a bot in chat ${chatName}`;
+    store.dispatch(sendMessage(id, BOT_NAME, content));
+}
+
+function generateBotMessageInNewChat (store, id) {
+    const chatName = store.getState().chats[id].name;
+    const content = `${name}, welcome to your new chat ${chatName}`;
     store.dispatch(sendMessage(id, BOT_NAME, content));
 }
