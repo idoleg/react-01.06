2951167b352fbce9627b0164e3364d7f89f6bd52
Evgeniya <ved-evgeniya@yandex.ru>
2020-06-25 14:11:02 +0300

    index

---
+++ EvgeniyaKarpova/src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, compose, applyMiddleware} from 'redux';
+import chatReducer from './chatReducer';
+import botMiddleware from './botMiddleware';
+import {createBrowserHistory} from 'history';
+import {routerMiddleware, connectRouter} from 'connected-react-router'
+
+export const history = createBrowserHistory();
+
+const reducer = combineReducers({
+    router: connectRouter(history),
+    chats: chatReducer,
+});
+
+export function initStore(preloadedState = undefined) {
+    const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+    
+    return createStore(reducer, preloadedState, composeEnhancers(applyMiddleware(
+        routerMiddleware(history),
+        botMiddleware
+    )));
+}
