4b558237d9e66f9b5bd287edc9adc3f543614dd6
Evgeniya <ved-evgeniya@yandex.ru>
2020-07-01 16:29:02 +0300

    lesson-8

---
+++ EvgeniyaKarpova/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ChatContainer from './containers/ChatContainer';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { initStore, history } from './store';
+import { ConnectedRouter } from 'connected-react-router';
+
+import {fetchChats} from './store/chatOperations';
+import {initChats} from './store/chatAction';
+
+const store = initStore();
+store.dispatch(fetchChats());
+
+export const App = () => (
+    <Provider store={store}>
+    <ConnectedRouter history={history}>
+        <Route path="/chats">  
+            <Switch>
+                <Route path="/chats" exact component={ChatContainer} />
+                <Route path="/chats/:id" exact component={ChatContainer} />
+            </Switch>    
+        </Route>
+    </ConnectedRouter>
+</Provider>
+)
