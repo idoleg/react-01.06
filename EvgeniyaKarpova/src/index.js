0e428955cb1f7c117a8f85a28ae48df5b10b0d75
Evgeniya <ved-evgeniya@yandex.ru>
2020-07-01 16:30:24 +0300

    lesson-8

--- EvgeniyaKarpova/src/index.js
+++ EvgeniyaKarpova/src/index.js
@@ -1,14 +1,15 @@
 import React, {createElement} from "react";
 import ReactDom from "react-dom";
-import MessagesList from "./components/MessagesList/MessagesList";
+import {MessagesList} from "./components/MessagesList/MessagesList";
+import {App} from "./App";
 
-const messages = [];
-// const messages = [
-//     {name: 'Лена', content: 'Превет!'},
-//     {name: 'Оля', content: 'Превет!'},
-//     {name: 'Лена', content: 'Как дела?'},
-//     {name: 'Оля', content: 'Хорошо'}
-// ]
+
+const messages = [
+    {name: 'Лена', content: 'Превет!'},
+    {name: 'Оля', content: 'Превет!'},
+    {name: 'Лена', content: 'Как дела?'},
+    {name: 'Оля', content: 'Хорошо'}
+]
 
 const mounPoint = document.getElementById('point');
-ReactDom.render(<MessagesList messages={messages}/>, mounPoint)
+ReactDom.render(<App/>, mounPoint)
